23101aed2d3cbf9aeed615de028be9de3ff0589a
Kingtouan1703 <redutdep13@gmail.com>
2022-04-23 15:38:37 +0700

    change component c

---
+++ src/ComponetC.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ComponetC() {
+    return (
+        <>
+            <div>ComponetC</div>
+            <div>Display</div>
+        </>
+    )
+}
+
+export default ComponetC
