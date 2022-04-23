12a77ddc6c4083bd09415de9b88e47bcfde42b95
Kingtouan1703 <redutdep13@gmail.com>
2022-04-23 16:03:57 +0700

    fix

--- src/ComponetC.js
+++ src/ComponetC.js
@@ -3,13 +3,8 @@ import React from 'react'
 function ComponetC() {
     return (
         <>
-<<<<<<< HEAD
             <div>new ComponetC</div>
             <div> new Display</div>
-=======
-            <div>make confict</div>
-            <div>make confict</div>
->>>>>>> 3a4b918544c0f6c1e698268314e5b175bcb7a8bb
         </>
     )
 }
